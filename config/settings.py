9922828e7a6957d70a8cfeecd4b4445426f14465
KRDevs <113543958+KRDevs@users.noreply.github.com>
2023-03-03 19:30:33 +0500

    This changing is for heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--u(lop)nu_#ruu^ou!*k)4sc*6(%@a^k(*$^dqf6ze^%1=zf=j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
